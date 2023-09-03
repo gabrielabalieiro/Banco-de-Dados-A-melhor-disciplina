383a0e299b0c370b361d33bf7b9bb4ec73d6beb9
gabrielabalieiro <gabibalieirosantos@gmail.com>
2023-09-03 18:47:35 -0300

    exercicio-3

--- listaExercicios01.sql
+++ listaExercicios01.sql
@@ -5,3 +5,9 @@ select titulo from livros;
 SELECT nome
 FROM autores
 WHERE nascimento < '1900-01-01';
+
+/* Exercicio 3 */
+select titulo 
+from livros
+inner join autores on livros.autor_id = autores.id
+where autores.nome = 'J.K. Rowling';
