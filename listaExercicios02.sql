d4f71f73eed2a1fe4a6bbd4fc864e3c7b4369856
gabrielabalieiro <99227467+gabrielabalieiro@users.noreply.github.com>
2023-09-25 21:38:38 -0300

    exercicio 2

--- listaExercicios02.sql
+++ listaExercicios02.sql
@@ -7,3 +7,20 @@ end //
 DELIMITER ;
 
 call sp_ListarAutores();
+
+/* Exercicio 2 */
+DELIMITER //
+create procedure sp_LivrosPorCategoria(in categoriaNome VARCHAR(100))
+begin
+    select Livro.Titulo, Autor.Nome, Autor.Sobrenome
+    from Livro
+    inner join Categoria on Livro.Categoria_ID = Categoria.Categoria_ID
+    inner join Autor_Livro on Livro.Livro_ID = Autor_Livro.Livro_ID
+    inner join Autor on Autor_Livro.Autor_ID = Autor.Autor_ID
+    where Categoria.Nome = categoriaNome;
+end //
+DELIMITER ;
+
+call sp_LivrosPorCategoria('Ciência');
+call sp_LivrosPorCategoria('Autoajuda');
+
