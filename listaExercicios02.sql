e21900a60d114be6bbe20a98e81eecd72537be40
gabrielabalieiro <99227467+gabrielabalieiro@users.noreply.github.com>
2023-09-25 22:13:50 -0300

    exercicio 9

--- listaExercicios02.sql
+++ listaExercicios02.sql
@@ -115,3 +115,18 @@ DELIMITER ;
 
 call sp_AutorMaisAntigo(@nomeAutorMaisAntigo);
 select @nomeAutorMaisAntigo;
+
+/* Exercicio 9 */ 
+DELIMITER //
+create  procedure sp_LivrosPorCategoria(in categoriaNome VARCHAR(100))
+begin
+    select Livro.Titulo, Autor.Nome, Autor.Sobrenome
+    from Livro
+    inner join Categoria on Livro.Categoria_ID = Categoria.Categoria_ID
+    inner join Autor_Livro on Livro.Livro_ID = Autor_Livro.Livro_ID
+    inner join Autor on Autor_Livro.Autor_ID = Autor.Autor_ID
+    where Categoria.Nome = categoriaNome;
+end //
+DELIMITER ;
+
+call sp_LivrosPorCategoria('Romance');
