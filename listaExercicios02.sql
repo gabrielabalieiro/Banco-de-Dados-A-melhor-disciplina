8cec86a4c6eee14441e3b6fb4e4cc5874daf765d
gabrielabalieiro <99227467+gabrielabalieiro@users.noreply.github.com>
2023-09-25 21:57:20 -0300

    exercicio 5

--- listaExercicios02.sql
+++ listaExercicios02.sql
@@ -53,3 +53,17 @@ DELIMITER ;
 
 call sp_VerificarLivrosCategoria('Romance', @possuiLivros);
 select @possuiLivros;
+
+/* Exercicio 5 */ 
+DELIMITER //
+create procedure sp_LivrosAteAno(in anoLimite INT)
+begin
+    select Livro.Titulo, Livro.Ano_Publicacao, Autor.Nome, Autor.Sobrenome
+    from Livro
+    inner join Autor_Livro on Livro.Livro_ID = Autor_Livro.Livro_ID
+    inner join Autor on Autor_Livro.Autor_ID = Autor.Autor_ID
+    where Livro.Ano_Publicacao <= anoLimite;
+end //
+DELIMITER ;
+
+call sp_LivrosAteAno(2005);
