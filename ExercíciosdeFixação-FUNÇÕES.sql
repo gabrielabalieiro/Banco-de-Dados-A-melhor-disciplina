84945d3874d81d3039caac8d9c50ac4973152fec
gabrielabalieiro <99227467+gabrielabalieiro@users.noreply.github.com>
2023-10-08 13:20:26 -0300

    exercicio-2

--- ExercíciosdeFixação-FUNÇÕES.sql
+++ ExercíciosdeFixação-FUNÇÕES.sql
@@ -19,3 +19,22 @@ select
         else CONCAT('Sr. ', nome)
     end as nome_formatado
 from nomes;
+
+
+/* Funções Numéricas */
+create table produtos (
+    produto VARCHAR(50),
+    preco DECIMAL(10, 2),
+    quantidade INT
+);
+insert into produtos (produto, preco, quantidade)
+values ('Produto 1', 19.99, 100),
+       ('Produto 2', 29.99, 75),
+       ('Produto 3', 9.99, 150);
+
+select produto, ROUND(preco, 2) as preco_arredondado from produtos;
+
+select produto, ABS(quantidade) as quantidade_absoluta from produtos;
+
+select AVG(preco) as media_preco from produtos;
+
