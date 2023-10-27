f942bdd8f8ecb0eec62c5f6788610b423455758a
gabrielabalieiro <99227467+gabrielabalieiro@users.noreply.github.com>
2023-10-26 22:11:03 -0300

    Update exerciciosDeFixacao20231026.sql

--- 4-bimestre/exerciciosDeFixacao20231026.sql
+++ 4-bimestre/exerciciosDeFixacao20231026.sql
@@ -15,26 +15,28 @@ AFTER UPDATE ON Clientes
 FOR EACH ROW 
 	INSERT INTO Auditoria (mensagem)
         VALUES ('Nome do cliente ' + OLD.nome + ' atualizado para ' + NEW.nome);
-
+delimiter //
 CREATE TRIGGER atualiza_nome_cliente_auditoria
 BEFORE UPDATE ON Clientes
 FOR EACH ROW
-delimiter //
+
     IF NEW.nome = NULL OR NEW.nome='' THEN
 		INSERT INTO Auditoria(mensagem, data_hora)
         values ('Há uma tentiva de atulização de cliente para nulo');
 
-	END IF //
+	END IF;
+//
 delimiter ;
-/*PROFESSOR  NÃO SEI OQUE ESTÁ DANDO ERRO DE SINTAXE  no (if)E NÃO SEI COMO ARRUMAR PROCUREI EN TODOS OS LUGARES E ELES TEM A MESMA SOLUÇÃO Q ESTÁ AI*/
 
+delimiter //
 CREATE TRIGGER novo_pedido_auditoria_estoque
 AFTER INSERT ON Pedidos
 FOR EACH ROW
     UPDATE Produtos SET estoque = estoque - NEW.quantidade WHERE id = NEW.produto_id;
-delimiter //
+
     IF (SELECT estoque FROM Produtos WHERE id = NEW.produto_id) < 5 THEN
         INSERT INTO Auditoria (mensagem)
         VALUES ('Estoque baixo para o produto com ID ' + NEW.produto_id);
-    END IF //
-    delimiter ;
+    END IF;
+ //
+delimiter ;
